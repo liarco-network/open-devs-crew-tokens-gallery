9e3ecb8993a281fb1dab019a8db1e16469f153d6
Zekel97 <szandreas97@gmail.com>
2023-01-18 17:15:31 +0100

    Adding github button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,44 @@
 import styles from '../styles/Home.module.scss'
 
 import type { NextPage } from 'next'
+import Head from 'next/head';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useDebounce } from 'use-debounce';
 import { BiWalletAlt } from 'react-icons/bi';
 import { useAccount } from 'wagmi';
-import Link from 'next/link';
 
 import TokensListComponent from '../components/TokensListComponent'
 import CustomConnectButton from '../components/CustomConnectButton';
 
 import OdcLogo from '../assets/images/odc-logo.png';
-import Head from 'next/head';
+import Github from '../assets/images/icons/github.svg';
 
 const Home: NextPage = () => {
   const [ collectionQuery, setCollectionQuery ] = useState("");
   const [ isUserConnected, setIsUserConnected ] = useState<boolean>();
+  const [githubButtonInView, setGithubButtonInView] = useState(false);
+
   const [ debouncedCollectionQuery ] = useDebounce(collectionQuery, 200);
   const { isConnected } = useAccount();
+
+  const handleGithubScroll = () => {
+    setGithubButtonInView(window.scrollY < window.innerHeight)
+  };
 
   useEffect(() => {
     setIsUserConnected(isConnected);
   },[isConnected]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleGithubScroll);
+
+    handleGithubScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleGithubScroll);
+    }
+  }, []);
 
   return (
     <>
@@ -31,6 +48,14 @@ const Home: NextPage = () => {
     </Head>
 
     <main className={styles.main}>
+        <a
+          id={styles.githubContainer}
+          className={githubButtonInView ? styles.githubButtonView : styles.githubButtonHidden}
+          href="https://github.com/liarco-network/open-devs-crew-tokens-gallery" target="_blank" rel="noreferrer"
+        >
+          <span>GitHub</span>
+          <Github />
+      </a>
       <div className={styles.titleBar}>
         <div className={styles.leftSide}>
           <Link href={'/'} passHref>
